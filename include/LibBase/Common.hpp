5e11f54ad443169c81fe15a7cd6d7095cd1e446a
icxd <icoding666@gmail.com>
2024-01-12 23:02:44 +0100

    LibBase: added varargs to PANIC macro

--- include/LibBase/Common.hpp
+++ include/LibBase/Common.hpp
@@ -3,12 +3,17 @@
 #include <LibBase/Debug.hpp>
 #include <LibC/stdint.h>
 
-#define PANIC(msg) panic(msg, __FILE__, __LINE__)
+#define PANIC(msg, ...) panic(msg, __FILE__, __LINE__, ##__VA_ARGS__)
 
 [[noreturn]] inline void panic(const char *message, const char *file,
-                               uint32_t line) {
+                               uint32_t line, ...) {
+    va_list ap;
+    va_start(ap, line);
     asm volatile("cli");
-    kprintf("PANIC(%s:%d): %s\n", file, line, message);
+    kprintf("PANIC(%s:%d):", file, line, message);
+    kvprintf(message, ap);
+    va_end(ap);
+
     while (1) {
         asm("hlt");
     }
