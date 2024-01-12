9b9f2e974efadf2bc7733e81aa253cf33591fd29
icxd <icoding666@gmail.com>
2024-01-12 23:21:55 +0100

    LibBase: updated NumericLimits for size_t to respect architecture

--- include/LibBase/NumericLimits.hpp
+++ include/LibBase/NumericLimits.hpp
@@ -7,6 +7,11 @@ template <typename T> struct NumericLimits {};
 
 template <> struct NumericLimits<size_t> {
     static constexpr size_t min() { return 0; }
-    static constexpr size_t max() { return UINT64_MAX; }
+    static constexpr size_t max() {
+        if constexpr (sizeof(size_t) == sizeof(uint32_t)) return UINT32_MAX;
+        else if constexpr (sizeof(size_t) == sizeof(uint64_t))
+            return UINT64_MAX;
+        else return 0;
+    }
     static constexpr bool is_signed() { return false; }
 };
