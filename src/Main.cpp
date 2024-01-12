84c684a0f86b3ee6490eb3a3e66b060d4968ce82
icxd <icoding666@gmail.com>
2024-01-12 23:23:22 +0100

    Main: move from kprintf to Logger class

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,6 @@
 #include <LibBase/Common.hpp>
 #include <LibBase/Debug.hpp>
+#include <LibBase/Logger.hpp>
 #include <LibC/stddef.h>
 #include <LibC/stdint.h>
 #include <LibC/stdio.h>
@@ -10,20 +11,20 @@ extern "C" void __cxa_pure_virtual() { PANIC("Pure virtual function called!"); }
 #define CHECK_FLAG(flags, bit) ((flags) & (1 << (bit)))
 
 extern "C" void kmain(uint32_t magic, uint32_t address) {
-    kputs("Hello, world!\n");
+    Logger::info("Hello, world!");
     multiboot_info_t *mbi;
 
     if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
-        PANIC("Invalid magic number!");
+        PANIC("Invalid magic number: 0x%X\n", (unsigned)magic);
     }
 
-    kprintf("magic = 0x%X\n", (unsigned)magic);
+    Logger::debug("magic = 0x%X", (unsigned)magic);
 
     mbi = (multiboot_info_t *)address;
-    kprintf("flags = 0x%X\n", (unsigned)mbi->flags);
+    Logger::debug("flags = 0b%b", (unsigned)mbi->flags);
 
     if (CHECK_FLAG(mbi->flags, 0)) {
-        kprintf("mem_lower = %uKB, mem_upper = %uKB\n",
-                (unsigned)mbi->mem_lower, (unsigned)mbi->mem_upper);
+        Logger::debug("mem_lower = %uKB, mem_upper = %uKB",
+                      (unsigned)mbi->mem_lower, (unsigned)mbi->mem_upper);
     }
 }
