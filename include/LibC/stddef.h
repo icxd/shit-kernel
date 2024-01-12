24fbf4c076bc2add9d146d30e0f17e683544c8a7
icxd <icoding666@gmail.com>
2024-01-12 23:02:10 +0100

    LibC: make size_t and ptrdiff_t aware if it's x86 or x64

--- include/LibC/stddef.h
+++ include/LibC/stddef.h
@@ -1,8 +1,14 @@
 #ifndef LIBC_STDDEF_H
 #define LIBC_STDDEF_H
 
-typedef unsigned long size_t;
+// TODO: upgrade to x64
+#if defined(__i386__)
+typedef int ptrdiff_t;
+typedef unsigned int size_t;
+#elif defined(__x86_64__)
 typedef long ptrdiff_t;
+typedef unsigned long size_t;
+#endif // __i386__
 
 #define NULL ((void *)0)
 
