76ca6cd9ae280e64bf2fc5b5ba035fdd1a76adde
icxd <icoding666@gmail.com>
2024-01-13 01:18:16 +0100

    Main: pretty print the mb flags :3

--- src/Main.cpp
+++ src/Main.cpp
@@ -10,21 +10,67 @@ extern "C" void __cxa_pure_virtual() { PANIC("Pure virtual function called!"); }
 
 #define CHECK_FLAG(flags, bit) ((flags) & (1 << (bit)))
 
+static const char *multiboot_flag_names[] = {
+    "Memory",
+    "Boot device",
+    "Command line",
+    "Modules",
+    "Aout symbol table",
+    "ELF sections",
+    "Memory map",
+    "Drive info",
+    "Config table",
+    "Boot loader name",
+    "APM table",
+    "VBE info",
+    "Framebuffer info",
+    "ELF symbols",
+    "Memory info",
+    "Multiboot info",
+    "Mmap address",
+    "Drives address",
+    "Config table address",
+    "Boot loader name address",
+    "APM table address",
+    "VBE info address",
+    "Framebuffer info address",
+    "Module address",
+    "Kernel start address",
+    "Kernel end address",
+    "Multiboot header address",
+};
+
 extern "C" void kmain(uint32_t magic, uint32_t address) {
-    Logger::info("Hello, world!");
+    Logger::info("Hello, world!\n");
     multiboot_info_t *mbi;
 
     if (magic != MULTIBOOT_BOOTLOADER_MAGIC) {
         PANIC("Invalid magic number: 0x%X\n", (unsigned)magic);
     }
 
-    Logger::debug("magic = 0x%X", (unsigned)magic);
+    Logger::debug("Magic = 0x%X\n", (unsigned)magic);
 
     mbi = (multiboot_info_t *)address;
-    Logger::debug("flags = 0b%b", (unsigned)mbi->flags);
+    Logger::debug("Flags = 0b%b\n", (unsigned)mbi->flags);
+    Logger::debug("  ");
+    size_t flag_count = 0;
+    for (int i = 0; i < 32; i++) {
+        if (CHECK_FLAG(mbi->flags, i)) {
+            flag_count++;
+            Logger::debug("%s, ", false, multiboot_flag_names[i]);
+
+            if (flag_count % 2 == 0) {
+                Logger::debug("\n", false);
+                Logger::debug("  ");
+            }
+        }
+    }
+    if (flag_count % 2 == 0) {
+        Logger::debug("\n", false);
+    }
 
     if (CHECK_FLAG(mbi->flags, 0)) {
-        Logger::debug("mem_lower = %uKB, mem_upper = %uKB",
-                      (unsigned)mbi->mem_lower, (unsigned)mbi->mem_upper);
+        Logger::debug("Lower memory = %uKB\n", (unsigned)mbi->mem_lower);
+        Logger::debug("Upper memory = %uKB\n", (unsigned)mbi->mem_upper);
     }
 }
